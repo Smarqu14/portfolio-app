2f4b8d89d4cc7e4114bb84f29a48bbbe37a9ed35
Steve Marquez <steveantonio00@gmail.com>
2021-01-01 19:14:15 -0800

    Add css for icons

--- src/components/About/TraitList.js
+++ src/components/About/TraitList.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { BsCodeSlash } from 'react-icons/bs';
+import { BsCloudUpload } from 'react-icons/bs';
+import { GrPersonalComputer } from 'react-icons/gr';
+import { AiOutlineInteraction } from 'react-icons/ai';
 
 const TraitList = () => {
-  const traits = ['Fast', 'Dynamic', 'Responsive', 'Interactive'];
+  const traits = [
+    {
+      trait: 'Fast',
+      description: 'Fast load times and lag free interaction.',
+      icon: <BsCodeSlash />,
+    },
+    {
+      trait: 'Dynamic',
+      description:
+        "Websites don't have to be static, I love making pages come to life.",
+      icon: <GrPersonalComputer />,
+    },
+    {
+      trait: 'Responsive',
+      description: 'My layouts will work on any device, big or small.',
+      icon: <BsCloudUpload />,
+    },
+    {
+      trait: 'Interactive',
+      description: 'Strong preference for easy to use, intuitive UX/UI.',
+      icon: <AiOutlineInteraction />,
+    },
+  ];
   return (
     <>
       <ul className='about__traits'>
-        {traits.map((trait) => {
-          return <li>{trait}</li>;
+        {traits.map((item) => {
+          return (
+            <li>
+              <div className='about__icon'>{item.icon}</div>
+              <div>
+                <h1>{item.trait}</h1>
+                <p>{item.description}</p>
+              </div>
+            </li>
+          );
         })}
       </ul>
     </>
