2c66e594558d6230bd1679459c29618c7a3fcc9e
Steve Marquez <steveantonio00@gmail.com>
2021-01-03 11:38:38 -0800

    Created PersonalInfo.js component

---
+++ src/components/About/PersonalInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PersonalInfo = () => {
+  return (
+    <div className='about__personal-info'>
+      <div className='about__picture' />
+      <div className='about__right'>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, nihil
+          distinctio, pariatur laboriosam saepe officiis, tenetur explicabo sit
+          placeat veritatis vero. Atque, nesciunt deserunt iste obcaecati
+          adipisci error consectetur earum? Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Vero quibusdam eaque esse officia
+          inventore rerum expedita culpa fugit veritatis dolorem, fuga quasi,
+          blanditiis Lorem
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TraitList from './TraitList';
+import PersonalInfo from './PersonalInfo';
 import myPhoto from '../../images/myphoto.JPG';
 
 const About = () => {
@@ -9,22 +10,7 @@ const About = () => {
         <h1>About</h1>
       </div>
       <TraitList />
-
-      <div className='test'>
-        <div className='about__picture' />
-
-        <div className='about__right'>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim,
-            nihil distinctio, pariatur laboriosam saepe officiis, tenetur
-            explicabo sit placeat veritatis vero. Atque, nesciunt deserunt iste
-            obcaecati adipisci error consectetur earum? Lorem ipsum, dolor sit
-            amet consectetur adipisicing elit. Vero quibusdam eaque esse officia
-            inventore rerum expedita culpa fugit veritatis dolorem, fuga quasi,
-            blanditiis Lorem
-          </p>
-        </div>
-      </div>
+      <PersonalInfo />
     </section>
   );
 };
