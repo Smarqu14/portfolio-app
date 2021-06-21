408e0d815adc3ea4661c1939e6e4e2ff3401e96c
Steve Marquez <steveantonio00@gmail.com>
2021-06-21 11:10:26 -0700

    Fix minor bug on projects setcion

--- src/components/About/PersonalInfo.js
+++ src/components/About/PersonalInfo.js
@@ -7,12 +7,19 @@ const PersonalInfo = () => {
       <div className='about__right'>
         <p>
           I am a driven Software Engineer with a professional background in
-          project management with a degree in Information Systems. To me, being
-          a Software Engineer is more than just a job or career. I consider it
-          an opportunity to showcase my technical skills and contribute to
-          something much more significant, the future of technology. Inspiring
-          the youth to never give up on their dreams has always pushed me to
-          become a better programmer and human being. I believe that I am
+          project management with a degree in Information Systems.
+        </p>
+        <br />
+        <p>
+          To me, being a Software Engineer is more than just a job or career. I
+          consider it an opportunity to showcase my technical skills and
+          contribute to something much more significant, the future of
+          technology.
+        </p>
+        <br />
+        <p>
+          Inspiring the youth to never give up on their dreams has always pushed
+          me to become a better programmer and human being. I believe that I am
           working towards making a change in the world; this is why I have
           become a Software Engineer.
         </p>
